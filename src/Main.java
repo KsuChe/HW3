5ee288622602c69ebcad13b0b26be2e6849b422f
KsuChe <nerro@mail.ru>
2022-04-22 22:47:20 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+            int IOS = 0;
+            boolean clientOSIos = IOS == 0;
+            if (clientOSIos) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                    }
+            int Android = 1;
+            boolean clientOsAndroid = Android == 1;
+            if (clientOsAndroid) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+                    }
 
-    }
-}
+            }
+
+        }
